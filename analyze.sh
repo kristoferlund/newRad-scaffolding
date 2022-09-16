97addc05ad5f059a0256b8c90800ae9b1eb02642
kristoferlund <kristofer@fmckl.se>
2022-09-16 10:44:58 +0200

    Rename report.sh analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! [ -d "$1" ] ; then
   echo "Could not find data folder";
-  echo "Usage: bash report.sh data_folder";
+  echo "Usage: bash analyze.sh data_folder";
   exit 1;
 fi
 pip install -r requirements.txt
