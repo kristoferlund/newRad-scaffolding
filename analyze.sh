854eebfde6b1334b778c5013d7c9bdb82c13ae91
kristoferlund <kristofer@fmckl.se>
2022-09-16 10:51:40 +0200

    Make pip more quiet

--- analyze.sh
+++ analyze.sh
@@ -4,5 +4,5 @@ if ! [ -d "$1" ] ; then
   echo "Usage: bash analyze.sh data_folder";
   exit 1;
 fi
-pip install -r requirements.txt
+pip install -r requirements.txt -q --disable-pip-version-check
 python main.py -p $1
