6bc0dafc789b5150be2af88bded3cfe5727cec4e
kristoferlund <kristofer@fmckl.se>
2022-09-16 09:24:19 +0200

    Check for existence of data folder

--- report.sh
+++ report.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+if ! [ -d "$1" ] ; then
+  echo "Could not find data folder";
+  echo "Usage: bash report.sh data_folder";
+  exit 1;
+fi
 pip install -r requirements.txt
-python main.py -p test_data/
+python main.py -p $1
